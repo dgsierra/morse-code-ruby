93cab0f1872566e9171db5c27506e4c550354b25
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-10-27 15:47:26 -0500

    Fix rubocop errors

--- main.rb
+++ main.rb
@@ -1,43 +1,31 @@
-messageWords = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...".split("   ")
-$dictionary = {
-  ".-" => "a",
-  "-..." => "b",
-  "-.-." => "c",
-  "-.." => "d",
-  "." => "e",
-  "..-." => "f",
-  "--." => "g",
-  "...." => "h",
-  ".." => "i",
-  ".---" => "j",
-  "-.-" => "k",
-  ".-.." => "l",
-  "--" => "m",
-  "-." => "n",
-  "---" => "o",
-  ".--." => "p",
-  "--.-" => "q",
-  ".-." => "r",
-  "..." => "s",
-  "-" => "t",
-  "..-" => "u",
-  "...-" => "v",
-  ".--" => "w",
-  "-..-" => "x",
-  "-.--" => "y",
-  "--.." => "z",
-}
+message_words = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'.split('   ')
 
-def decodeLetter(letters)
-  $dictionary[letters].upcase
+def dec_char(letters)
+  dictionary = {
+    '.-' => 'a',
+    '-...' => 'b',
+    '-.-.' => 'c',
+    '-..' => 'd',
+    '.' => 'e',
+    '..-.' => 'f',
+    '--.' => 'g',
+    '....' => 'h',
+    '..' => 'i',
+    '.---' => 'j',
+    '-.-' => 'k', '.-..' => 'l', '--' => 'm', '-.' => 'n',
+    '---' => 'o', '.--.' => 'p',
+    '--.-' => 'q', '.-.' => 'r', '...' => 's', '-' => 't',
+    '..-' => 'u', '...-' => 'v', '.--' => 'w', '-..-' => 'x', '-.--' => 'y', '--..' => 'z'
+  }
+  dictionary[letters].upcase
 end
 
-def decodeWord(word)
-  word.split(" ").map { |letter| decodeLetter(letter) }.join
+def decode_word(word)
+  word.split.map { |letter| dec_char(letter) }.join
 end
 
-def decodePhrase(message)
-  message.map { |word| decodeWord(word) }.join(" ")
+def decode_phrase(message)
+  message.map { |word| decode_word(word) }.join(' ')
 end
 
-puts decodePhrase(messageWords)
+puts decode_phrase(message_words)
